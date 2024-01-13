e7e3729e2d00019ae89eb819c04d26bc0dda8fbd
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2024-01-13 12:33:08 +0530

    [React] Adding more styling to the Profile Card Apps

--- programming/react/src/pdas-demo-1/src/App.js
+++ programming/react/src/pdas-demo-1/src/App.js
@@ -7,21 +7,36 @@ import SiriImage from "./images/siri.png";
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
       <div className="container">
         <div className="section">
           <div className="columns">
             <div className="column is-4">
-              <ProfileCard title="Siri" handle="@apple3000" image={SiriImage} />
+              <ProfileCard
+                title="Siri"
+                handle="@apple3000"
+                image={SiriImage}
+                description="Created by Apple"
+              />
             </div>
             <div className="column is-4">
-              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="created by Amazon"
+              />
             </div>
             <div className="column is-4">
               <ProfileCard
                 title="Google"
                 handle="@google"
                 image={GoogleImage}
+                description="created by google"
               />
             </div>
           </div>
