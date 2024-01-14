7a1ef68e63adcd0a17e4e407ee381d28b08cf451
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2024-01-14 10:01:50 +0530

    [Namaste React] Inception

---
+++ programming/react/nmste-rjs/src/1_helloWorld/App.js
@@ -0,0 +1,19 @@
+
+// 1) Creating a h1 tag by using core React APIs
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World! from React"
+);
+
+const paragraph = React.createElement("p", {id: "paragraph"}, "I am a paragraph")
+
+const child = React.createElement("div", {id: "child"}, [heading, paragraph]);
+const child2 = React.createElement("div", {id: "child2"}, [heading, paragraph]);
+
+const parent = React.createElement("div", {id: "parent"}, [child, child2])
+//2) Creating the root element.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//3) Rendering the heading inside root
+root.render(parent);
